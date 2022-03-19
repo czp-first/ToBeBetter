59fe2eb8879b61736a75bd0882097380255c7f98
czp <czp_first@163.com>
2022-03-19 22:38:10 +0800

    binary search

---
+++ algorithm/binary_search/binary_search.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 -*-
+"""
+@Summary : 二分查找, leecode: 704
+@Author  : Rey
+@Time    : 2022-03-19 22:06:36
+"""
+
+def binary_search(wait_list, target) -> int:
+    """二分查找
+
+    :param list wait_list: 待查列表
+    :param _type_ target: 目标元素
+    :return int: 目标位置
+    """
+
+    left = 0
+    right = len(wait_list) - 1
+
+    while left <= right:
+        cur = (left + right) // 2
+        if wait_list[cur] == target:
+            return cur
+        
+        if wait_list[cur] < target:
+            left = cur + 1
+        else:
+            right = cur - 1
+    
+    if wait_list[left] == target:
+        return left
+    
+    return -1
